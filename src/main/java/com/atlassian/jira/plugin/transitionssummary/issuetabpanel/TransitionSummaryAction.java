6413d5d72042929e2d9dbb1f1157fecbec7cca79
abashev <none@none>
2007-09-06 15:48:53 +0400

    [svn r95] Fixes for issue #21
    --HG--
    branch : trunk

--- src/main/java/com/atlassian/jira/plugin/transitionssummary/issuetabpanel/TransitionSummaryAction.java
+++ src/main/java/com/atlassian/jira/plugin/transitionssummary/issuetabpanel/TransitionSummaryAction.java
@@ -1,30 +1,28 @@
 package com.atlassian.jira.plugin.transitionssummary.issuetabpanel;
 
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.ofbiz.core.entity.GenericValue;
 import org.ofbiz.core.util.UtilMisc;
-import com.atlassian.jira.issue.Issue;
-//import com.atlassian.jira.issue.action.AbstractGVIssueAction;  // Corrected for 3.7
+
+import com.atlassian.core.user.UserUtils;
 import com.atlassian.jira.plugin.issuetabpanel.AbstractIssueAction;
 import com.atlassian.jira.plugin.issuetabpanel.IssueTabPanelModuleDescriptor;
+import com.atlassian.jira.plugin.transitionssummary.TransitionSummary;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
-import com.opensymphony.user.User;
+import com.opensymphony.user.EntityNotFoundException;
 
 /**
  * @author Gustavo Martin
  * 
  * This is a valid Action, that it allows to visualize the Transition Summaries.
- *  
  */
-public class TransitionSummaryAction extends AbstractIssueAction{
-	
+public class TransitionSummaryAction extends AbstractIssueAction {
 	protected final IssueTabPanelModuleDescriptor descriptor;
-	protected List tranSummaries;
+	protected List<TransitionSummary> tranSummaries;
 	protected Timestamp timePerformed;
 	
 	/**
@@ -33,8 +31,9 @@ public class TransitionSummaryAction extends AbstractIssueAction{
 	 * @param tranSummaries List containing TransitionSummary objects.
 	 * @param descriptor
 	 */
-	public TransitionSummaryAction(List tranSummaries, IssueTabPanelModuleDescriptor descriptor){
+	public TransitionSummaryAction(List<TransitionSummary> tranSummaries, IssueTabPanelModuleDescriptor descriptor){
 		super(descriptor);
+		
 		this.tranSummaries = tranSummaries;
 		this.descriptor = descriptor;
 		this.timePerformed = new Timestamp(Calendar.getInstance().getTimeInMillis());
@@ -45,53 +44,37 @@ public class TransitionSummaryAction extends AbstractIssueAction{
 	 * 
 	 * It allows Velocity to obtain the List of Transition Summaries.
 	 */
-	public List getTransitions() {
+	public List<TransitionSummary> getTransitions() {
 		return tranSummaries;
 	}
 	
 	/* (non-Javadoc)
-	 * @see com.atlassian.jira.issue.action.AbstractIssueAction#getTemplateName()
-	 */
-	//protected String getTemplateName() {
-		//return null;
-	//}
-	
-	/* (non-Javadoc)
-	 * @see com.atlassian.jira.issue.action.IssueAction#getType()
-	 */
-	//public String getType() {
-		//return "plugin.transitionssummary.transitionsummary";
-	//}
-	
-	/* (non-Javadoc)
-	 * @see com.atlassian.jira.issue.action.IssueAction#getUsername()
-	 */
-	//public String getUsername() {
-		//return "";
-	//}
-	
-	/* (non-Javadoc)
-	 * @see com.atlassian.jira.issue.action.IssueAction#getTimePerformed()
-	 */
-	/* (non-Javadoc)
 	 * @see com.atlassian.jira.issue.action.IssueAction#getTimePerformed()
 	 */
 	public Date getTimePerformed() {
-		return (Date)this.timePerformed;
+		return (Date) this.timePerformed;
 	}
 	
 	/* (non-Javadoc)
 	 * @see com.atlassian.jira.issue.action.IssueAction#getHtml(com.atlassian.jira.web.action.JiraWebActionSupport)
 	 */
-	public String getHtml(JiraWebActionSupport webAction)
-	{
+	public String getHtml(JiraWebActionSupport webAction) {
 		Map params = UtilMisc.toMap("webAction", webAction, "action", this);
+		
 		return descriptor.getHtml("view", params);
 	}
 	
-	protected void populateVelocityParams(Map params)
-	{	
-		//System.out.println("Call to populateVelocityParams"); // Debug output
+	protected void populateVelocityParams(Map params) {	
 		params.put("action", this);
+	}
+	
+	public boolean isUserExists(String username) {
+		try {
+			UserUtils.getUser(username);
+		} catch (EntityNotFoundException e) {
+			return false;
+		}
+		
+		return true;
 	}
 }
