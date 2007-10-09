fb2db417e4ef31c3b0709cee64dc4273cd651277
abashev <none@none>
2007-10-09 17:53:05 +0400

    [svn r113] Updated logger category for new package
    --HG--
    branch : trunk

--- src/main/java/com/googlecode/jsu/util/LogUtils.java
+++ src/main/java/com/googlecode/jsu/util/LogUtils.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Logger;
  * @author Alexey Abashev
  */
 public final class LogUtils {
-	private static final Logger GENERAL = Logger.getLogger("com.atlassian.plugin.suite-utilities");
+	private static final Logger GENERAL = Logger.getLogger("com.googlecode.jsu");
 	
 	public static Logger getGeneral() {
 		return GENERAL;
