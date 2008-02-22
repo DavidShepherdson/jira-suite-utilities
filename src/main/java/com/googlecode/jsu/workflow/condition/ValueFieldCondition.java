ae644b835d70f15e4baf2a8443c110a3532d556f
abashev <none@none>
2008-02-22 23:26:10 +0300

    [svn r128] Issue #36
    --HG--
    branch : trunk

--- src/main/java/com/googlecode/jsu/workflow/condition/ValueFieldCondition.java
+++ src/main/java/com/googlecode/jsu/workflow/condition/ValueFieldCondition.java
@@ -14,20 +14,18 @@ import com.googlecode.jsu.util.WorkflowUtils;
 import com.opensymphony.module.propertyset.PropertySet;
 
 /**
- * @author Gustavo Martin
- * 
  * This condition evaluates if a given field fulfills the condition.
- *  
+ * 
+ * @author Gustavo Martin
  */
 public class ValueFieldCondition extends AbstractJiraCondition {
-	private Issue issueObject = null;
-	
+
 	/* (non-Javadoc)
 	 * @see com.opensymphony.workflow.Condition#passesCondition(java.util.Map, java.util.Map, com.opensymphony.module.propertyset.PropertySet)
 	 */
 	public boolean passesCondition(Map transientVars, Map args, PropertySet ps) {
-		this.issueObject = getIssue(transientVars);
-		
+		Issue issue = getIssue(transientVars);
+
 		String sField = (String) args.get("fieldsList");
 		String fieldCondition = (String) args.get("conditionList");
 		String comparisonType = (String) args.get("comparisonType");
@@ -37,29 +35,30 @@ public class ValueFieldCondition extends AbstractJiraCondition {
 		Field field = WorkflowUtils.getFieldFromKey(sField);
 		ComparisonType compType = ComparisonManager.getManager().getComparisonType(new Integer(comparisonType));
 		ConditionType cond = ConditionManager.getManager().getCondition(new Integer(fieldCondition));
-		
-		return checkCondition(field, cond.getValue(), compType.getValue(), fieldValue);
+
+		return checkCondition(issue, field, cond.getValue(), compType.getValue(), fieldValue);
 	}
-	
+
 	/**
+	 * This method make the evaluation properly this. If the comparison is by
+	 * String, only equal or different are valid options for condition.
+	 * 
+	 * @param issue
 	 * @param field
 	 * @param condition
 	 * @param comparisonType
 	 * @param fieldValue
 	 * @return true if fulfills the condition, false otherwise.
-	 * 
-	 * This method make the evaluation properly this.
-	 * If the comparisson is by String, only equal or different are valid options for condition.
 	 */
-	private boolean checkCondition(Field field, String condition, String comparisonType, String fieldValue) {
+	private boolean checkCondition(Issue issue, Field field, String condition, String comparisonType, String fieldValue) {
 		boolean condOK = false;
-		
+
 		// If the comparisson is by String.
 		if (comparisonType.equals(WorkflowUtils.COMPARISON_TYPE_STRING)) {
-			String originalValue = WorkflowUtils.getFieldValueFromIssueAsString(issueObject, field);
-			
+			String originalValue = WorkflowUtils.getFieldValueFromIssueAsString(issue, field);
+
 			condOK = originalValue.equals(fieldValue);
-			
+
 			if (condition.equals("=")) {
 				condOK = true && condOK;
 			} else {
@@ -69,22 +68,22 @@ public class ValueFieldCondition extends AbstractJiraCondition {
 					condOK = false;
 				}
 			}
-		} else if (comparisonType.equals(WorkflowUtils.COMPARISON_TYPE_NUMBER)){
+		} else if (comparisonType.equals(WorkflowUtils.COMPARISON_TYPE_NUMBER)) {
 			int comparison = -111;
 
-			Object value = WorkflowUtils.getFieldValueFromIssue(issueObject, field);
+			Object value = WorkflowUtils.getFieldValueFromIssue(issue, field);
 
-			try{
+			try {
 				if (fieldValue.trim().equals("")) {
 					if (value == null) {
 						comparison = 0;
 					}
 				} else {
 					Double expectingValue = Double.valueOf(fieldValue);
-					
+
 					if (value != null) {
 						Double numberValue;
-						
+
 						if (value instanceof String) {
 							numberValue = Double.valueOf((String) value);
 						} else if (value instanceof Number) {
@@ -92,7 +91,7 @@ public class ValueFieldCondition extends AbstractJiraCondition {
 						} else {
 							throw new NumberFormatException();
 						}
-						
+
 						comparison = expectingValue.compareTo(numberValue);
 					}
 				}
@@ -105,33 +104,32 @@ public class ValueFieldCondition extends AbstractJiraCondition {
 				LogUtils.getGeneral().error("Wrong number format", nfe);
 				comparison = -111;
 			}
-			
+
 			// If there were no errors, verifies that the comparison between the fields 
 			// is same that the condition passed like parameter.
 			if (comparison != (-111)) {
-				
+
 				if ((comparison == 0) && ((condition.equals("<=")) || (condition.equals("=")) || (condition.equals(">="))))
 					condOK = true;
-				
+
 				if ((comparison < 0) && ((condition.equals("<")) || (condition.equals("<="))))
 					condOK = true;
-				
+
 				if ((comparison > 0) && ((condition.equals(">")) || (condition.equals(">="))))
 					condOK = true;
-				
+
 				if ((comparison != 0) && (condition.equals(WorkflowUtils.CONDITION_DIFFERENT)))
 					condOK = true;
-				
+
 			}
 
-		} else if(comparisonType.equals(WorkflowUtils.COMPARISON_TYPE_DATE)){
+		} else if (comparisonType.equals(WorkflowUtils.COMPARISON_TYPE_DATE)) {
 			// Not implemented yet.
 			//Timestamp originalValue = (Timestamp)WorkflowUtils.getFieldValueFromIssue(issueObject, field);
-			
+
 			//comparison = originalValue.compareTo(Timestamp.valueOf(fieldValue));
 		}
-		
+
 		return condOK;
 	}
 }
-
