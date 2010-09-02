1c377b3f627c471cd47970b8a46f4ce4012a9e94
abashev <abashev@gmail.com>
2010-09-03 00:23:56 +0400

    Issue 67
    --HG--
    branch : trunk

--- src/main/java/com/googlecode/jsu/util/CommonPluginUtils.java
+++ src/main/java/com/googlecode/jsu/util/CommonPluginUtils.java
@@ -339,7 +339,6 @@ public class CommonPluginUtils {
 	 */
 	private static List<Field> getNonRequirableFields(){
 		return asFields(
-				COMMENT,
 				CREATED,
 				TIMETRACKING,
 				TIME_ORIGINAL_ESTIMATE,
